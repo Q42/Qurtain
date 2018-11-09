826427a9c0fd2e357febc52d74e710d05dd12980
karsveling <kars@q42.nl>
2018-11-09 23:00:00 +0100

    audio upd

--- new_server.js
+++ new_server.js
@@ -92,23 +92,57 @@ function boot() {
 
     // mount mic
     var mic = require('mic');
+    var WavDecoder = require('wav-decoder');
     
     var micInstance = mic({
-      rate: '16000',
+      rate: '8000',
       channels: '1',
+      bitwidth: 8,
       debug: true,
-      exitOnSilence: 6
+      exitOnSilence: 3
     });
 
     console.log("mounting mic");
     var micInputStream = micInstance.getAudioStream();
       
-    micInputStream.on('data', function(data) {
-      console.log("Recieved Input Stream: " + data.length);
+    const minTime = 500; // ms
+    const threshold = 0.7;
+    let time = null;
+    let buffers = [];
+    
+    micInputStream.on('data', buffer => {
+      const newTime = new Date().getTime(); 
+      buffers.push(buffer); // -> save previous recorded data
+      if(newTime - time > minTime) { // -> start do something if min time pass
+        const headerBuf = header(config.rate, config); // ->  create wav header
+        buffers.unshift(headerBuf); // -> set header in top of buffers
+        const length = _.sum(buffers.map(b => b.length));
+        
+        WavDecoder.decode(Buffer.concat(buffers, length)) // -> decode buffers to float array
+          .then(audioData => {
+            const wave = audioData.channelData[0];
+            const maxAmplitude = _.max(wave);
+            if (maxAmplitude > threshold) {
+            console.log('-----> clap'); // -> any logic here
+            }
+          })
+          .catch(console.log);
+        time = newTime; // -> reset the timer
+        buffers = []; // free recorded data
+      }
     });
+
+    // micInputStream.on('data', function(data) {
+    //   console.log("Recieved Input Stream: " + data.length);
+
+    // });
 
     micInputStream.on('silence', function() {
       console.log("silence");
+    });
+
+    micInputStream.on('speech', function() {
+      console.log("speech");
     });
 
     //var outputFileStream = fs.WriteStream('output.raw');
