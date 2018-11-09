a1c1cbe690ad8694bf899189af788dc719a16b5f
Michiel Post <michiel@q42.nl>
2018-11-09 16:49:17 +0100

    native location

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var ws281x = require('../lib/ws281x-native');
+var ws281x = require('../rpi_ws281x');
 
 var NUM_LEDS = parseInt(process.argv[2], 10) || 10,
     pixelData = new Uint32Array(NUM_LEDS);
