e7b0a9174538452099f2eb8b3de52632c23e4276
Laurens van den Oever <laurens@q42.nl>
2018-11-10 03:43:06 +0100

    Better wiggle

--- image.js
+++ image.js
@@ -25,7 +25,7 @@ function start(screen) {
                         var offset = 0
                         setInterval(function() {
                             const matrix = utils.createMatrix(WIDTH, HEIGHT);
-                            const wiggle = Math.round(Math.sin(offset/WIGGLE_LENGTH) * 8)
+                            const wiggle = Math.round(Math.sin(offset/WIGGLE_LENGTH) * 7 - (IMG_W/2 - WIDTH/2))
                             utils.drawImage(matrix, arr, wiggle, (offset++ % (HEIGHT+IMG_H)) -IMG_H)
                             const pixels = utils.matrixToPixels(matrix)
                             screen.render(pixels);
