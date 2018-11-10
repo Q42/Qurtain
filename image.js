8bc87413c727726fa4b8cdf10c03fd8b3f48d81f
Laurens van den Oever <laurens@q42.nl>
2018-11-10 10:27:05 +0100

    Subpixel rendering

--- image.js
+++ image.js
@@ -4,37 +4,50 @@ const utils = require('./utils')
 const WIDTH = 5
 const HEIGHT = 150
 const WIGGLE_LENGTH = 10*Math.PI
+const SCALE = 5
 
 async function start(screen) {
 
     const IMG_W = 15
     const IMG_H = 72
 
-    const img = await loadImage('./imgs/q42-logo-groot-on-black.png', IMG_W, IMG_H)
+    const logo = await loadImage('./imgs/q42-logo-groot-on-black.png', IMG_W*SCALE, IMG_H*SCALE)
     var offset = 0
-    setInterval(() => {
-        const matrix = utils.createMatrix(WIDTH, HEIGHT);
+    setInterval(async () => {
+        const canvas = await newImage(WIDTH*SCALE, HEIGHT*SCALE)
         const wiggle = Math.round(Math.sin(offset/WIGGLE_LENGTH) * 7 - (IMG_W/2 - WIDTH/2))
-        utils.drawImage(matrix, img, wiggle, (offset++ % (HEIGHT+IMG_H)) -IMG_H)
-        const pixels = utils.matrixToPixels(matrix)
-        screen.render(pixels);
+        canvas.blit(logo, wiggle*SCALE, ((offset++ % (HEIGHT+IMG_H)) -IMG_H)* SCALE )
+        const pixels = canvasToPixels(canvas)
+        screen.render(pixels)
     }, 1000 / 60)
 }
 
 async function loadImage(path, width, height) {
     const img = await Jimp.read(path)
-    const arr = utils.createMatrix(width, height)
-    const small = img.resize(width, height)
-    small.scan(0, 0, width, height, (x, y, idx) => {
-        const r = img.bitmap.data[idx + 0]
-        const g = img.bitmap.data[idx + 1]
-        const b = img.bitmap.data[idx + 2]
-        const a = img.bitmap.data[idx + 3]
+    const resized = img.resize(width, height)
 
-        arr[y][x] = r<<16|g<<8|b
+    return resized
+}
+
+function canvasToPixels(canvas) {
+    const w = WIDTH
+    const h = HEIGHT
+    const matrix = utils.createMatrix(w, h)
+    canvas.resize(w, h).scan(0, 0, w, h, (x, y, idx) => {
+        const r = canvas.bitmap.data[idx + 0]
+        const g = canvas.bitmap.data[idx + 1]
+        const b = canvas.bitmap.data[idx + 2]
+        const a = canvas.bitmap.data[idx + 3]
+
+        matrix[y][x] = r<<16|g<<8|b
     })
 
-    return arr
+    const pixels = utils.matrixToPixels(matrix)
+    return pixels
+}
+
+async function newImage(width, height) {
+    return await new Jimp(width, height)
 }
 
 module.exports.start = start;
